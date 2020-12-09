92a270ebd9ace63be08d112f9005638100d76aaa
Sivakumar-U <usivakumarreddy@gmail.com>
2020-12-09 16:45:01 +0530

    Refactored: Code to view employee payroll details from local storage

--- js/Utility.js
+++ js/Utility.js
@@ -1,5 +1,5 @@
-const stringyfyDate = (date) => {
+const stringifyDate = (date) => {
     const options = { day: 'numeric', month: 'short', year: 'numeric' };
-    const newDate = !date ? "undefined" : new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
+    const newDate = !date ? "undefined" : new Date(Date.parse(date)).toLocaleDateString('en-US', options);
     return newDate;
 }
--- js/employee_home.js
+++ js/employee_home.js
@@ -3,7 +3,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
     empPayrollList = getEmployeePayrollDataFromStorage();
     document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
-    localStorage.removeItem('editEmp');
 });
 
 const getEmployeePayrollDataFromStorage = () => {
@@ -13,19 +12,19 @@ const getEmployeePayrollDataFromStorage = () => {
 
 //Template literal ES6 feature
 const createInnerHtml = () => {
-    if(empPayrollList.length == 0) return;
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
                         "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    if(empPayrollList.length == 0) return;
     let innerHtml = `${headerHtml}`;
     for ( const empPayrollData of empPayrollList) {
         innerHtml = `${innerHtml}
         <tr>
-            <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+            <td><img class="profile" alt="" src="${empPayrollData._picture}"></td>
             <td>${empPayrollData._name}</td>
             <td>${empPayrollData._gender}</td>
             <td>${getDeptHtml(empPayrollData._department)}</td>
             <td>${empPayrollData._salary}</td>
-            <td>${stringyfyDate(empPayrollData._startDate)}</td>
+            <td>${stringifyDate(empPayrollData._startDate)}</td>
             <td>
                 <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
                 <img id="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
@@ -34,6 +33,10 @@ const createInnerHtml = () => {
     `;
     }
         document.querySelector('#table-display').innerHTML = innerHtml;
+};
+
+function remove(empPayrollList) {
+    localStorage.removeItem(empPayrollList);
 }
 
 const getDeptHtml = (deptList) => {
@@ -42,4 +45,4 @@ const getDeptHtml = (deptList) => {
         deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
     }
     return deptHtml;
-}
+};
